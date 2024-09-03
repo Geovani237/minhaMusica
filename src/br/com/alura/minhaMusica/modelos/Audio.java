f7ae874b712a531bcfb1afead483bca19743bf35
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2024-09-03 09:47:23 -0300

    Criação da classe Audio

---
+++ src/br/com/alura/minhaMusica/modelos/Audio.java
@@ -0,0 +1,8 @@
+package br.com.alura.minhaMusica.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private double classficacao;
+}

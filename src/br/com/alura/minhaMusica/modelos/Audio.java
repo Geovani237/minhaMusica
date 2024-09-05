4aad501020622b92c75ad8564556b8b852c392d7
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2024-09-05 09:32:44 -0300

    trabalhando com herança
    Adicionando classes, "Musica" e  "Podcast", ambas estendendo da superclasse "Audio". Também foi criado a classe "Principal"  e feito alguns testes

--- src/br/com/alura/minhaMusica/modelos/Audio.java
+++ src/br/com/alura/minhaMusica/modelos/Audio.java
@@ -4,5 +4,32 @@ public class Audio {
     private String titulo;
     private int totalReproducoes;
     private int totalCurtidas;
-    private double classficacao;
+    private int classficacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public int getClassficacao() {
+        return classficacao;
+    }
+
+    public void curte() {
+        this.totalCurtidas++;
+    }
+    public void reproduz(){
+        this.totalReproducoes++;
+    }
 }
---
+++ src/br/com/alura/minhaMusica/modelos/principal/Principal.java
@@ -0,0 +1,32 @@
+package br.com.alura.minhaMusica.modelos.principal;
+
+import br.com.alura.minhaMusica.modelos.Musica;
+import br.com.alura.minhaMusica.modelos.Podcast;
+
+public class Principal {
+    public static void main (String[] args) {
+        Musica musica = new Musica();
+        musica.setTitulo("Hey Jude");
+        musica.setCantor("The Beatle");
+
+        for(int i = 0; i < 1000; i++) {
+            musica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            musica.curte();
+        }
+
+        Podcast podcast = new Podcast();
+        podcast.setTitulo("BolhaDev");
+        podcast.setApresentador("Marcus Mendes");
+
+        for(int i = 0; i < 5000; i++) {
+            podcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            podcast.curte();
+        }
+    }
+}
